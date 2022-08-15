30523c5c53d98ac5c0f3f0e22313440b97e63643
Adith A Danthi <adith.danthi@gmail.com>
2022-08-15 22:43:02 +0530

    feat: setup HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const webpackConfig = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    mode: 'production',
-}
+  entry: path.resolve(__dirname, "src", "index.js"),
+
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack Starter",
+      template: path.resolve(__dirname, "src", "index.html"),
+    }),
+  ],
+  mode: "production",
+};
 
 module.exports = webpackConfig;
